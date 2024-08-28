5bf0c8c2b0f357962f30923dd60ab3c7c8fb9f1b
thectogeneral <salemolorundare@gmail.com>
2024-08-28 13:29:40 +0100

    rate limiter

--- limiter.py
+++ limiter.py
@@ -16,15 +16,15 @@ class RateLimiter:
     def encode_key(self, value):
         return value.encode('utf-8').hex() if self.hash_key else value
 
-    def is_request_allowed(self, key):
-        redis_key = self.key_prefix + self.encode_key(key)
+    def is_request_allowed(self, ip):
+        redis_key = self.key_prefix + self.encode_key(ip)
         last_refill_time_key = redis_key + self.last_refill_prefix
 
         current_tokens = self.redis_client.get(redis_key)
         if current_tokens is None:
             current_tokens = self.max_tokens
         else:
-            current_tokens = int(current_tokens)
+            current_tokens = float(current_tokens)
 
         last_refill_time = self.redis_client.get(last_refill_time_key)
         if last_refill_time is None:
